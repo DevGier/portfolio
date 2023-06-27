3db5db50a41ccff9a0fadb6c923ccb8281cd1f17
tim de gier <timdegier@MacBook-Air-de-tim.local>
2023-06-27 23:38:24 +0200

    Hero fixes

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -10,34 +10,42 @@
 </script>
 
 <section class="hero">
-    <div class="flex w-full z-10 relative h-full text-white">
-     
-        <div class="w-full container mx-auto my-auto relative flex gap-8">
+  <div class="flex w-full z-10 relative h-full text-white">
+    <div class="w-full container mx-auto my-auto relative flex gap-8">
+      <div
+        class="flex flex-col justify-center items-center mt-[-56px] relative"
+      >
+        <div class="w-5 h-5 relative z-10 rounded-full bg-indigo-500" />
+        <div
+          class="absolute top-[50%] w-1 h-[200px] bg-gradient-to-b from-indigo-500 to-transparent ..."
+        />
+      </div>
 
-          <div class="flex flex-col justify-center items-center mt-[-56px] relative"><div class="w-5 h-5 relative z-10 rounded-full bg-indigo-500"></div><div class="absolute top-[50%] w-1 h-[400px] bg-gradient-to-b from-indigo-500 to-transparent ..."></div></div>
+      <div>
+        <h1 class="text-7xl relative z-10 font-bold">
+          Hi, I'm <span class="text-indigo-500">Tim</span>
+        </h1>
+        <p class="text-white relative z-10 pt-[24px] text-2xl">
+          I'm a Frontend Developer
+        </p>
 
-          <div>
-            <h1 class="text-7xl relative z-10 font-bold">Hi, I'm <span class="text-indigo-500">Tim</span></h1>
-            <p class="text-white relative z-10 pt-[24px] text-2xl">Frontend developer</p>
-
-            <canvas id="herocanvas" class="relative z-0 hidden md:block"></canvas>
-          </div>
-            
-        </div>
+        <canvas id="herocanvas" class="relative z-0 hidden md:block" />
+      </div>
     </div>
+  </div>
 
-    <div class="absolute bottom-16 z-20 w-full text-center">
-      <a href="#about">
-        <div id="mouse-scroll">
-          <div class="mouse">
-            <div class="mouse-in"></div>
-          </div>
-          <div>
-              <span class="down-arrow-1"></span>
-              <span class="down-arrow-2"></span>
-              <span class="down-arrow-3"></span>
-           </div>
+  <div class="absolute bottom-16 z-20 w-full text-center">
+    <a href="#about">
+      <div id="mouse-scroll">
+        <div class="mouse">
+          <div class="mouse-in" />
         </div>
-        </a>
-    </div>
+        <div>
+          <span class="down-arrow-1" />
+          <span class="down-arrow-2" />
+          <span class="down-arrow-3" />
+        </div>
+      </div>
+    </a>
+  </div>
 </section>
