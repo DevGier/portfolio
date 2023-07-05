bf2aebdb396b8976a1796dbf6650b5b509793cf3
Tim de Gier <timdegier@MBP-Tim.local>
2023-07-05 15:45:30 +0200

    Moved some stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-	import Header from './../components/Header.svelte';
-    import '../index.css';
-    import '../styles/main.scss';
+  import '../index.css'
+  import '../styles/main.scss'
+  import Header from './../components/Header.svelte'
 </script>
 
-<Header/>
+<Header />
 <slot />
